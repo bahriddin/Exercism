b687fd4cd34054205cddc2937197be348657e418
bahriddin <bahriddin.abdiev@ferocia.com.au>
2021-06-19 19:16:09 +1000

    Improve Resistor Color Duo''

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,12 +1,21 @@
-export class ResistorColor {
-  private colors: string[];
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
 
-  constructor(colors: string[]) {
+export class ResistorColor {
+  constructor(private colors: string[]) {
     if (colors.length < 2) {
       throw new Error("At least two colors need to be present");
     }
-
-    this.colors = colors;
   }
 
   value = (): number =>
@@ -14,29 +23,6 @@ export class ResistorColor {
     this._colorToNumber(this.colors[1]);
 
   _colorToNumber = (color: string): number => {
-    switch (color) {
-      case "black":
-        return 0;
-      case "brown":
-        return 1;
-      case "red":
-        return 2;
-      case "orange":
-        return 3;
-      case "yellow":
-        return 4;
-      case "green":
-        return 5;
-      case "blue":
-        return 6;
-      case "violet":
-        return 7;
-      case "grey":
-        return 8;
-      case "white":
-        return 9;
-      default:
-        throw new Error("Incorrect color is provided");
-    }
+    return COLORS.indexOf(color);
   };
 }
