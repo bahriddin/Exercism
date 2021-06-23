59e37a3714bef3ca2a3fbc13b5f0a75b4ebca9c5
bahriddin <bahriddin.abdiev@ferocia.com.au>
2021-06-23 13:25:23 +1000

    Refactor 'Space Age'

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,55 +1,23 @@
-const EARTH_SECONDS = 31557600;
-const ORBITAL_PERIODS = {
-  earth: 1,
-  mercury: 0.2408467,
-  venus: 0.61519726,
-  mars: 1.8808158,
-  jupiter: 11.862615,
-  saturn: 29.447498,
-  uranus: 84.016846,
-  neptune: 164.79132,
-};
+enum Planets {
+  Earth = 31_557_600,
+  Mercury = 0.2408467 * Earth,
+  Venus = 0.61519726 * Earth,
+  Mars = 1.8808158 * Earth,
+  Jupiter = 11.862615 * Earth,
+  Saturn = 29.447498 * Earth,
+  Uranus = 84.016846 * Earth,
+  Neptune = 164.79132 * Earth,
+}
 
 export default class SpaceAge {
-  constructor(public seconds: number) {}
+  [key: string]: any;
 
-  onEarth = () =>
-    this._roundTwoDigits(
-      this.seconds / (ORBITAL_PERIODS.earth * EARTH_SECONDS)
-    );
+  constructor(public readonly seconds: number) {
+    for (const planet in Planets) {
+      this[`on${planet}`] = () => this._calculate(<any>Planets[planet]);
+    }
+  }
 
-  onMercury = () =>
-    this._roundTwoDigits(
-      this.seconds / (ORBITAL_PERIODS.mercury * EARTH_SECONDS)
-    );
-
-  onVenus = () =>
-    this._roundTwoDigits(
-      this.seconds / (ORBITAL_PERIODS.venus * EARTH_SECONDS)
-    );
-
-  onMars = () =>
-    this._roundTwoDigits(this.seconds / (ORBITAL_PERIODS.mars * EARTH_SECONDS));
-
-  onJupiter = () =>
-    this._roundTwoDigits(
-      this.seconds / (ORBITAL_PERIODS.jupiter * EARTH_SECONDS)
-    );
-
-  onSaturn = () =>
-    this._roundTwoDigits(
-      this.seconds / (ORBITAL_PERIODS.saturn * EARTH_SECONDS)
-    );
-
-  onUranus = () =>
-    this._roundTwoDigits(
-      this.seconds / (ORBITAL_PERIODS.uranus * EARTH_SECONDS)
-    );
-
-  onNeptune = () =>
-    this._roundTwoDigits(
-      this.seconds / (ORBITAL_PERIODS.neptune * EARTH_SECONDS)
-    );
-
-  _roundTwoDigits = (num: number) => Math.round(num * 100) / 100;
+  _calculate = (planet_year_seconds: Planets): number =>
+    Math.round((this.seconds / planet_year_seconds) * 100) / 100;
 }
